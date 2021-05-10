e778ebfbaa55617d823d56a23672587599f78f71
William Arin <williamarin.dev@gmail.com>
2021-05-10 22:18:33 +0800

    build: remove corejs dependency

--- webpack.config.js
+++ webpack.config.js
@@ -21,10 +21,6 @@ Encore
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(Encore.isProduction())
-    .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = 'usage';
-        config.corejs = 3;
-    })
     .enableSassLoader()
     .enableTypeScriptLoader()
     .addRule({
