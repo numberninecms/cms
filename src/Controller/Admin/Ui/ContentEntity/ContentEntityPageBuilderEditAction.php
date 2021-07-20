6e6da1c29b2dad9dccd72022ba9d1d7dcc389249
William Arin <williamarin.dev@gmail.com>
2021-07-20 21:45:32 +0800

    feat: page builder for content entities

--- src/Controller/Admin/Ui/ContentEntity/ContentEntityPageBuilderEditAction.php
+++ src/Controller/Admin/Ui/ContentEntity/ContentEntityPageBuilderEditAction.php
@@ -12,8 +12,11 @@
 namespace NumberNine\Controller\Admin\Ui\ContentEntity;
 
 use NumberNine\Content\ContentService;
+use NumberNine\Content\ShortcodeProcessor;
+use NumberNine\Content\ShortcodeStore;
 use NumberNine\Entity\ContentEntity;
 use NumberNine\Model\Admin\AdminController;
+use NumberNine\Model\Shortcode\EditableShortcodeInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -30,15 +33,25 @@ final class ContentEntityPageBuilderEditAction extends AbstractController implem
 {
     public function __invoke(
         ContentService $contentService,
+        ShortcodeStore $shortcodeStore,
+        ShortcodeProcessor $shortcodeProcessor,
         ContentEntity $entity,
         string $type
     ): Response {
         $contentType = $contentService->getContentType($type);
+        $shortcodes = [];
+
+        foreach ($shortcodeStore->getShortcodes() as $name => $shortcode) {
+            if (is_subclass_of($shortcode, EditableShortcodeInterface::class)) {
+                $shortcodes[$name] = $shortcodeProcessor->shortcodeToArray($name);
+            }
+        }
 
         return $this->render('@NumberNine/admin/content_entity/builder_edit.html.twig', [
             'content_type' => $contentType,
             'type_slug' => $type,
             'entity' => $entity,
+            'shortcodes' => $shortcodes,
         ]);
     }
 }
