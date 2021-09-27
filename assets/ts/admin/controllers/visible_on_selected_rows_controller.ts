e14d209167053f1019676d5a47c7d043aa14b555
William Arin <williamarin.dev@gmail.com>
2021-09-27 14:17:42 +0800

    feat(admin): hide bulk action buttons when no row is selected

---
+++ assets/ts/admin/controllers/visible_on_selected_rows_controller.ts
@@ -0,0 +1,27 @@
+/*
+ * This file is part of the NumberNine package.
+ *
+ * (c) William Arin <williamarin.dev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { Controller } from 'stimulus';
+
+export default class extends Controller {
+    public connect(): void {
+        const form = (this.element as HTMLElement).closest('form');
+        const rows = form?.querySelectorAll('.select-row input');
+
+        rows?.forEach((checkbox) => {
+            checkbox.addEventListener('input', () => {
+                if (Array.from(rows).filter((c) => (c as HTMLInputElement).checked).length > 0) {
+                    (this.element as HTMLElement).classList.remove('hidden');
+                } else {
+                    (this.element as HTMLElement).classList.add('hidden');
+                }
+            });
+        });
+    }
+}
