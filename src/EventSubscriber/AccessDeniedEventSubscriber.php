292be403ee48aa34533451573381c60b4b494bbe
William Arin <williamarin.dev@gmail.com>
2021-10-01 15:12:20 +0800

    refactor: fix return types deprecations

--- src/EventSubscriber/AccessDeniedEventSubscriber.php
+++ src/EventSubscriber/AccessDeniedEventSubscriber.php
@@ -23,7 +23,7 @@ class AccessDeniedEventSubscriber implements EventSubscriberInterface
     {
     }
 
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
             ExceptionEvent::class => 'handleAccessDenied',
