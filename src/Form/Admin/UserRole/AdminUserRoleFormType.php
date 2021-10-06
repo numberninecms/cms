9bddcbb1228cd955709a5a7bf8ad17cd0f783069
William Arin <williamarin.dev@gmail.com>
2021-10-07 00:32:46 +0800

    feat(AdminUserRoleFormType): add validation rules

--- src/Form/Admin/UserRole/AdminUserRoleFormType.php
+++ src/Form/Admin/UserRole/AdminUserRoleFormType.php
@@ -18,13 +18,14 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 final class AdminUserRoleFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', null, ['constraints' => new NotBlank()])
             ->add('create', SubmitType::class, ['attr' => ['value' => 'create']])
         ;
     }
